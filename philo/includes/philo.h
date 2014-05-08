616439b816d10b2b8b34876ba88c53a582fa67f6
Wouter <wouter@Beets-Gautier.local>
2014-05-08 22:40:34 +0200

    boo

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -9,11 +9,16 @@
 # define THINKING		1
 # define EATING			2
 # define DEAD			3
+# define FULL			1
+# define EMPTY			0
 
 typedef struct		s_philo
 {
-	int		status;
-	int		life;
-	int		left_hand;
-	int		right_hand;
+	int				status;
+	int				num;
+	int				life;
+	int				left_hand;
+	int				right_hand;
+	struct s_philo	*left_ph;
+	struct s_philo	*right_ph;
 }					t_philo;
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -3,46 +3,100 @@
 #include <stdlib.h>
 #include <math.h>
 #include "philo.h"
+#include "libft.h"
+#include <unistd.h>
 
-void		philo(void *data)
+pthread_mutex_t wisemen[NUM_THREADS];
+
+void		*philo(void *data)
 {
-   int i;
-   pthread_exit((void*) i);
+	t_philo		*ph;
+
+	ph =(t_philo *)data;
+	while (42)
+	{
+		ft_putstr("im alive");
+		sleep(1);
+		pthread_mutex_lock(&wisemen[ph->num]);
+		ph->life -= 1;
+		if (ph->life <= 0)
+		{
+			ph->status = DEAD;
+			pthread_exit((void *)ph);
+		}
+		pthread_mutex_unlock(&wisemen[ph->num]);
+	}
+}
+
+t_philo		**init_ph(void)
+{
+	int		i;
+	t_philo	**ph;
+
+	ph = (t_philo **)malloc(NUM_THREADS * sizeof(t_philo *));
+	i = 0;
+	while (i < NUM_THREADS)
+	{
+		ph[i] = (t_philo *)malloc(sizeof(t_philo));
+		pthread_mutex_init(&wisemen[i], NULL);
+		i++;
+	}
+	i = 0;
+	while (i < NUM_THREADS)
+	{
+		ph[i]->status = RESTING;
+		ph[i]->left_hand = FULL;
+		ph[i]->right_hand = EMPTY;
+		if (i == 0)
+			ph[i]->left_ph = ph[NUM_THREADS - 1];
+		else 
+			ph[i]->left_ph = ph[i - 1];
+		if (i == NUM_THREADS)
+			ph[i]->right_ph = ph[0];
+		else
+			ph[i]->right_ph = ph[i];
+		ph[i]->life = MAX_LIFE;
+		ph[i]->num = i;
+		i++;
+	}
+	return (ph);
 }
 
 int main ()
 {
-   pthread_t thread[NUM_THREADS];
-   pthread_attr_t attr;
-   int rc;
-   long i;
-   void *status;
-   t_philo	ph[NUM_THREADS];
+	pthread_t		thread[NUM_THREADS];
+	t_philo			**ph;
+	pthread_attr_t	attr;
+	int				rc;
+	int				i;
+	void			*status;
 
-   /* Initialize and set thread detached attribute */
-   pthread_attr_init(&attr);
-   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
-   for(i=0; i<NUM_THREADS; i++) {
-      printf("Main: creating thread %ld\n", i);
-      rc = pthread_create(&thread[i], &attr, philo, (void *)i); 
-      if (rc) {
-         printf("ERROR; return code from pthread_create() is %d\n", rc);
-         exit(-1);
-         }
-      }
-
-   /* Free attribute and wait for the other threads */
-   pthread_attr_destroy(&attr);
-   for(i=0; i<NUM_THREADS; i++) {
-      rc = pthread_join(thread[i], &status);
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	ph = init_ph();
+	i = 0;
+	while(i < NUM_THREADS)
+	{
+		printf("Main: creating thread %d\n", i);
+		rc = pthread_create(&thread[i], &attr, philo, (void *)ph[i]); 
+		if (rc)
+		{
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			exit(-1);
+		}
+		i++;
+	}
+	pthread_attr_destroy(&attr);
+	for(i=0; i<NUM_THREADS; i++)
+	{
+		rc = pthread_join(thread[i], &status);
       if (rc) {
          printf("ERROR; return code from pthread_join is %d\n", rc);
          exit(-1);
          }
-      printf("Main: completed join with thread %ld having a status of %ld\n",i,(long)status);
+      printf("Main: completed join with thread number %d\n", ((t_philo *)status)->num );
       }
  
-printf("Main: program completed. Exiting.\n");
-pthread_exit(NULL);
+	printf("Main: program completed. Exiting.\n");
+	pthread_exit(NULL);
 }
